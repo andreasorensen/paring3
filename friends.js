e66428eecd94f1b44705a836700e256d003b496e
Andrea Sorensen <dreasor2987@icloud.com>
2023-03-02 21:03:15 -0800

    Add a new program

--- friends.js
+++ friends.js
@@ -1,7 +1,13 @@
 var nums = [7, 10, 21, 32, 38, 55, 63, 77];
 
+// for (var i=0; i < 100; i++) {
+//     if(nums[i]>30){
+//         console.log(nums[i]);
+//     }
+// }
+
 for (var i=0; i < 100; i++) {
-    if(nums[i]>30){
+    if(nums[i]<60){
         console.log(nums[i]);
     }
 }
